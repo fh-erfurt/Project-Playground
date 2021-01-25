73d81d70505cf52b110826c42033cc839a739095
Marvin Ruppert <MarvinRuppert2@gmx.de>
2021-01-25 16:50:12 +0100

    Todo hinzugefügt
    Co-Authored-By: M4rkN07 <65530893+M4rkN07@users.noreply.github.com>
    Co-Authored-By: Katja Fischer <65532051+Katja39@users.noreply.github.com>
    Co-Authored-By: fabianseeber <65532036+fabianseeber@users.noreply.github.com>

--- src/test/java/projectplayground/BenutzerTest.java
+++ src/test/java/projectplayground/BenutzerTest.java
@@ -85,5 +85,6 @@ class BenutzerTest  {
     }
     //ToDo freundhinzufügen, freundEntfernen, spielplatzFavoritHinzufuegen,spielplatzFavoritEntfernen
     //ToDO bestehende Testfunktionen auch überprüfen
+    //Todo Testdaten überprüfen
 
 }
