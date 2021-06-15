df64bc2ab4893cea349877992e6ef43af4f297b4
Marvin Ruppert <MarvinRuppert2@gmx.de>
2021-06-15 10:49:45 +0200

    Component verschoben

--- src/main/java/projectplayground/Application.java
+++ src/main/java/projectplayground/Application.java
@@ -1,10 +1,30 @@
 package projectplayground;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+
+import javax.faces.webapp.FacesServlet;
+import javax.servlet.ServletContext;
+import java.util.Arrays;
 
 @SpringBootApplication
 public class Application {
     public static void main(String[]args){
         SpringApplication.run(Application.class,args);//SpringBootAnwendung
     }
+
+    @Bean
+    ServletRegistrationBean<?> jsfServletRegistration(ServletContext servletContext) {
+        //spring boot only works if this is set
+        servletContext.setInitParameter("com.sun.faces.forceLoadConfiguration", Boolean.TRUE.toString());
+
+        //registration
+        final ServletRegistrationBean<FacesServlet> srb = new ServletRegistrationBean();
+        srb.setServlet(new FacesServlet());
+        srb.setUrlMappings(Arrays.asList("*.xhtml"));
+        srb.setLoadOnStartup(1);
+        return srb;
+    }
+
 }
