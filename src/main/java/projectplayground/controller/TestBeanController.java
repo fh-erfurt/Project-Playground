e0b46131f7667093263f685da4e287ad7d7def35
Marvin Ruppert <MarvinRuppert2@gmx.de>
2021-06-15 11:38:26 +0200

    aktueller stand

--- src/main/java/projectplayground/controller/TestBeanController.java
+++ src/main/java/projectplayground/controller/TestBeanController.java
@@ -2,8 +2,11 @@ package projectplayground.controller;
 
 
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.bean.ViewScoped;
 
 @ManagedBean
+@RequestScoped
 public class TestBeanController {
 
     public String showHello()
